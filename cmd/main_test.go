bd166dbeb76c144263fd9c3e73fb92af84283d33
Hristov, Simeon <Simeon.Hristov@experian.com>
2025-12-11 13:08:24 +0200

    Add failing test: when all arguments passed then should return them.

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestParseArgs(t *testing.T) {
 	t.Run("when nothing set then should return default values", func(t *testing.T) {
@@ -16,7 +19,32 @@ func TestParseArgs(t *testing.T) {
 
 		// Assert
 		if actual != expected {
-			t.Fatalf("\nexpected=\n%#v\nactual=\n%#v\n", actual, expected)
+			t.Fatalf("\nexpected=\n%#v\nactual=\n%#v\n", expected, actual)
+		}
+	})
+
+	t.Run("when all arguments passed then should return them", func(t *testing.T) {
+		// Arrange
+		day := 12
+		part := 2
+		input := "tmp1.txt"
+
+		expected := Config{
+			Day:   day,
+			Part:  part,
+			Input: input,
+		}
+
+		// Act
+		actual := ParseArgs([]string{
+			fmt.Sprintf("-day=%d", day),
+			fmt.Sprintf("-part=%d", part),
+			fmt.Sprintf("-input=%s", input),
+		})
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nexpected=\n%#v\nactual=\n%#v\n", expected, actual)
 		}
 	})
 }
