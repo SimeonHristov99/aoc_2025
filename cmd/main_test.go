be67ebd1afeb6f6411764b937d5195b40de4f1ed
Hristov, Simeon <Simeon.Hristov@experian.com>
2025-12-11 12:34:29 +0200

    Fix test.

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,20 +1,18 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestParseArgs(t *testing.T) {
 	t.Run("when nothing set then should return default values", func(t *testing.T) {
 		// Arrange
 		expected := Config{
-			Day: 1,
-			Part: 1,
+			Day:   1,
+			Part:  1,
 			Input: "internal/day01/part01/input.txt",
 		}
 
 		// Act
-		actual := ParseArgs()
+		actual := ParseArgs([]string{})
 
 		// Assert
 		if actual != expected {
