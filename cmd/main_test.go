a0e0da4422ffbc9fb2a6e1a2cd85f05d5c493412
Hristov, Simeon <Simeon.Hristov@experian.com>
2025-12-11 15:38:50 +0200

    Add failing test: when called then returns result of day 1 part 1 with input.

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"testing"
 )
+
+func captureStdout(f func()) string {
+	r, w, _ := os.Pipe()
+	old := os.Stdout
+	os.Stdout = w
+	f()
+	_ = w.Close()
+	os.Stdout = old
+	b, _ := io.ReadAll(r)
+	_ = r.Close()
+	return string(b)
+}
 
 func TestParseArgs(t *testing.T) {
 	t.Run("when nothing set then should return default values", func(t *testing.T) {
@@ -105,6 +119,29 @@ func TestParseArgs(t *testing.T) {
 		}
 		if !strings.Contains(actualErrorMessage, expectedErrorMessage) {
 			t.Fatalf("\nexpected=\n%#v\nactual=\n%#v\n", expectedErrorMessage, actualErrorMessage)
+		}
+	})
+}
+
+func TestMain(t *testing.T) {
+	t.Run("when called then returns result of day 1 part 1 with input", func(t *testing.T) {
+		// Arrange
+		day := 1
+		part := 1
+		input := "internal/day01/part01/input.txt"
+		result := 0
+		expectedOutput := fmt.Sprintf("Day %d, Part=%d, Input='%s': %d\n", day, part, input, result)
+
+		initialArgs := os.Args
+		defer func() { os.Args = initialArgs }()
+		os.Args = []string{initialArgs[0], fmt.Sprintf("-day=%d", day), fmt.Sprintf("-part=%d", part), fmt.Sprintf("-input=%s", input)}
+
+		// Act
+		actualOutput := captureStdout(main)
+
+		// Assert
+		if actualOutput != expectedOutput {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actualOutput, expectedOutput)
 		}
 	})
 }
