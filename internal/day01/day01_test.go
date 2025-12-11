d5ba94b7e34dad7ce68c8ecaace9a84d296a6db1
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-11 16:35:00 +0200

    Add failing test: when file does not exist then raise an error.

--- internal/day01/day01_test.go
+++ internal/day01/day01_test.go
@@ -1,16 +1,43 @@
 package day01
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestSolvePart1(t *testing.T) {
 	t.Run("when called then returns 0", func(t *testing.T) {
 		// Arrange
 		expected := 0
+		var expectedError error = nil
+		file := "internal/day01/sample.txt"
 
 		// Act
-		actual := SolvePart1()
+		actual, actualError := SolvePart1(file)
 
 		// Assert
+		if actualError != expectedError {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actualError, expectedError)
+		}
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+
+	t.Run("when file does not exist then raise an error", func(t *testing.T) {
+		// Arrange
+		file := "something.txt"
+		expected := 0
+		expectedError := fmt.Sprintf("File %s does not exist.", file)
+
+		// Act
+		actual, err := SolvePart1(file)
+		actualError := err.Error()
+
+		// Assert
+		if actualError != expectedError {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actualError, expectedError)
+		}
 		if actual != expected {
 			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
 		}
