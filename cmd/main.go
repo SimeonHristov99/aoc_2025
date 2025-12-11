79982e21ddd5c435b1c58aeaa8a1ff7b9a648c94
Hristov, Simeon <Simeon.Hristov@experian.com>
2025-12-11 13:42:01 +0200

    Make test pass.

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,16 @@ type Config struct {
 	Input string
 }
 
-func ParseArgs(args []string) Config {
+func ParseArgs(args []string) (Config, error) {
 	var config Config
-	fs := flag.NewFlagSet("aoc_2025", flag.PanicOnError)
+	fs := flag.NewFlagSet("aoc_2025", flag.ContinueOnError)
 	fs.IntVar(&config.Day, "day", 1, "which day to run")
 	fs.IntVar(&config.Part, "part", 1, "which part to run")
 	fs.StringVar(&config.Input, "input", "internal/day01/part01/input.txt", "path to input file")
-	fs.Parse(args)
-	return config
+	if err := fs.Parse(args); err != nil {
+		return Config{}, err
+	}
+	return config, nil
 }
 
 func main() {
