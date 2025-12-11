2981362321fd66a2d70ddc0fedb3c34d876d6506
Hristov, Simeon <Simeon.Hristov@experian.com>
2025-12-11 12:27:51 +0200

    Add failing test.

---
+++ cmd/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParseArgs(t *testing.T) {
+	t.Run("when nothing set then should return default values", func(t *testing.T) {
+		// Arrange
+		expected := Config{
+			Day: 1,
+			Part: 1,
+			Input: "internal/day01/part01/input.txt",
+		}
+
+		// Act
+		actual := ParseArgs()
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("expected=\n%#v\nactual=\n%#v\n", expected, actual)
+		}
+	})
+}
