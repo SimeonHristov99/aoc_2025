a6185a69ca5deba0ea34b5c1dfe41adad0c8a5c7
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-13 17:00:38 +0200

    Make test pass.

--- internal/day01/day01.go
+++ internal/day01/day01.go
@@ -1,5 +1,12 @@
 package day01
 
+import "strconv"
+
+func ProcessSingle(input string) int {
+	i, _ := strconv.Atoi(input[1:])
+	return -1 * i
+}
+
 func SolvePart1(filepath string) (int, error) {
 	return 0, nil
 }
