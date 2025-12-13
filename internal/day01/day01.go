534106adb92e022703128e75ae5e16a15764178f
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-13 17:20:32 +0200

    Make test pass.

--- internal/day01/day01.go
+++ internal/day01/day01.go
@@ -1,6 +1,10 @@
 package day01
 
-import "strconv"
+import (
+	"os"
+	"strconv"
+	"strings"
+)
 
 func ProcessSingle(input string) int {
 	i, _ := strconv.Atoi(input[1:])
@@ -8,6 +12,11 @@ func ProcessSingle(input string) int {
 		return -1 * i
 	}
 	return i
+}
+
+func ParseInput(filename string) []string {
+	contents, _ := os.ReadFile(filename)
+	return strings.Split(string(contents), "\n")
 }
 
 func SolvePart1(filepath string) (int, error) {
