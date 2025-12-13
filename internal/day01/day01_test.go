b6baa6b2d3bb3720b0c9c8b251044ed9eed4cf23
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-13 17:20:22 +0200

    Add failing test: when called then returns array of strings.

--- internal/day01/day01_test.go
+++ internal/day01/day01_test.go
@@ -1,6 +1,25 @@
 package day01
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseInput(t *testing.T) {
+	t.Run("when called then returns array of strings", func(t *testing.T) {
+		// Arrange
+		filename := "sample.txt"
+		expected := []string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"}
+
+		// Act
+		actual := ParseInput(filename)
+
+		// Assert
+		if reflect.DeepEqual(actual, expected) {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+}
 
 func TestProcessSingle(t *testing.T) {
 	t.Run("when called with left and single digit then returns negative digit", func(t *testing.T) {
