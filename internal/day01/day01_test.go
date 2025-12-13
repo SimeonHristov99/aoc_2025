de0a9e0d182b23d243ce0f883b4ac56be0cf6b73
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-13 17:00:25 +0200

    Add failing test: when called with left and single digit then returns negative digit.

--- internal/day01/day01_test.go
+++ internal/day01/day01_test.go
@@ -2,6 +2,22 @@ package day01
 
 import "testing"
 
+func TestProcessSingle(t *testing.T) {
+	t.Run("when called with left and single digit then returns negative digit", func(t *testing.T) {
+		// Arrange
+		input := "L5"
+		expected := -5
+
+		// Act
+		actual := ProcessSingle(input)
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+}
+
 func TestSolvePart1(t *testing.T) {
 	t.Run("when called then returns 0", func(t *testing.T) {
 		// Arrange
