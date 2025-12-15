4af24c2c7138b7672de89314092feee963af33d2
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-15 16:36:16 +0200

    Make test pass.

--- internal/day03/day03.go
+++ internal/day03/day03.go
@@ -9,3 +9,14 @@ func parseInput(filepath string) []string {
 	contents, _ := os.ReadFile(filepath)
 	return strings.Fields(string(contents))
 }
+
+func findMaxDigit(numStr string) int {
+	maxDigit := numStr[0] - '0'
+	numDigits := len(numStr)
+	for i := 1; i < numDigits; i++ {
+		if maxDigit < numStr[i] {
+			maxDigit = numStr[i]
+		}
+	}
+	return int(maxDigit - '0')
+}
