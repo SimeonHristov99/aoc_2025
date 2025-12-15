0b7ab3294dcb523eabfc02e14a3087257c0cf804
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-15 14:54:26 +0200

    Add failing test: when called with sample then returns parsed input.

---
+++ internal/day02/day02_test.go
@@ -0,0 +1,34 @@
+package day02
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestParseInput(t *testing.T) {
+	t.Run("when called with sample then returns parsed input", func(t *testing.T) {
+		// Arrange
+		filename := "sample.txt"
+		expected := [][2]int{
+			[2]int{11, 22},
+			[2]int{95, 115},
+			[2]int{998, 1012},
+			[2]int{1188511880, 1188511890},
+			[2]int{222220, 222224},
+			[2]int{1698522, 1698528},
+			[2]int{446443, 446449},
+			[2]int{38593856, 38593862},
+			[2]int{565653, 565659},
+			[2]int{824824821, 824824827},
+			[2]int{2121212118, 2121212124},
+		}
+
+		// Act
+		actual := parseInput(filename)
+
+		// Assert
+		if !reflect.DeepEqual(actual, expected) {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+}
