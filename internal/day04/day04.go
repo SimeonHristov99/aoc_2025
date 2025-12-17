ae44e1012b0bc1b9f6897fe03c068f38aa5d3eb1
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-17 17:01:18 +0200

    Reuse to solve part 1.

--- internal/day04/day04.go
+++ internal/day04/day04.go
@@ -49,33 +49,5 @@ func getAccessibleIndices(rolls []string) [][2]int {
 }
 
 func SolvePart1(filepath string) (int, error) {
-	paperRolls := parseInput(filepath)
-	numRows := len(paperRolls)
-	numCols := len(paperRolls[0])
-	numAccessible := 0
-	for i := 0; i < numRows; i++ {
-		for j := 0; j < numCols; j++ {
-			if paperRolls[i][j] != '@' {
-				continue
-			}
-
-			adjacentRolls := 0
-			for k := -1; k <= 1; k++ {
-				for l := -1; l <= 1; l++ {
-					if k == l && l == 0 {
-						continue
-					}
-
-					if coordsAreValid(i+k, j+l, numRows, numCols) && paperRolls[i+k][j+l] == '@' {
-						adjacentRolls += 1
-					}
-				}
-			}
-
-			if adjacentRolls < 4 {
-				numAccessible++
-			}
-		}
-	}
-	return numAccessible, nil
+	return len(getAccessibleIndices(parseInput(filepath))), nil
 }
