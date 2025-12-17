085827c720dc67d7511660837c47ac82784b2543
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-17 16:26:02 +0200

    Add more failing tests.

--- internal/day04/day04_test.go
+++ internal/day04/day04_test.go
@@ -49,4 +49,89 @@ func TestCoordsAreValid(t *testing.T) {
 			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
 		}
 	})
+
+	t.Run("when row equal to number of rows then returns false", func(t *testing.T) {
+		// Arrange
+		row := 10
+		col := 5
+		numRows := 10
+		numCols := 6
+		expected := false
+
+		// Act
+		actual := CoordsAreValid(row, col, numRows, numCols)
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+
+	t.Run("when row more than number of rows then returns false", func(t *testing.T) {
+		// Arrange
+		row := 15
+		col := 5
+		numRows := 10
+		numCols := 6
+		expected := false
+
+		// Act
+		actual := CoordsAreValid(row, col, numRows, numCols)
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+
+	t.Run("when column negative then returns false", func(t *testing.T) {
+		// Arrange
+		row := 5
+		col := -5
+		numRows := 10
+		numCols := 6
+		expected := false
+
+		// Act
+		actual := CoordsAreValid(row, col, numRows, numCols)
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+
+	t.Run("when column equal to number of columns then returns false", func(t *testing.T) {
+		// Arrange
+		row := 5
+		col := 6
+		numRows := 10
+		numCols := 6
+		expected := false
+
+		// Act
+		actual := CoordsAreValid(row, col, numRows, numCols)
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+
+	t.Run("when column more than number of columns then returns false", func(t *testing.T) {
+		// Arrange
+		row := 5
+		col := 15
+		numRows := 10
+		numCols := 6
+		expected := false
+
+		// Act
+		actual := CoordsAreValid(row, col, numRows, numCols)
+
+		// Assert
+		if actual != expected {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
 }
