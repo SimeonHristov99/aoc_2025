cb2ed2766ded5fa5750f810ce1d081cf8dafb921
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-17 16:59:52 +0200

    Make test pass.

--- internal/day04/day04.go
+++ internal/day04/day04.go
@@ -15,6 +15,39 @@ func coordsAreValid(row int, col int, numRows int, numCols int) bool {
 	return 0 <= row && row < numRows && 0 <= col && col < numCols
 }
 
+func getAccessibleIndices(rolls []string) [][2]int {
+	accessibleIndices := [][2]int{}
+
+	numRows := len(rolls)
+	numCols := len(rolls[0])
+	for i := 0; i < numRows; i++ {
+		for j := 0; j < numCols; j++ {
+			if rolls[i][j] != '@' {
+				continue
+			}
+
+			adjacentRolls := 0
+			for k := -1; k <= 1; k++ {
+				for l := -1; l <= 1; l++ {
+					if k == l && l == 0 {
+						continue
+					}
+
+					if coordsAreValid(i+k, j+l, numRows, numCols) && rolls[i+k][j+l] == '@' {
+						adjacentRolls += 1
+					}
+				}
+			}
+
+			if adjacentRolls < 4 {
+				accessibleIndices = append(accessibleIndices, [2]int{i, j})
+			}
+		}
+	}
+
+	return accessibleIndices
+}
+
 func SolvePart1(filepath string) (int, error) {
 	paperRolls := parseInput(filepath)
 	numRows := len(paperRolls)
