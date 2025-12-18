a92f5d0d01939e7d9eb80cef1d7b4674ed9f29ed
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-18 16:38:32 +0200

    Add failing test: when in range then returns true.

--- internal/day05/day05_test.go
+++ internal/day05/day05_test.go
@@ -10,7 +10,7 @@ func TestParseInput(t *testing.T) {
 		// Arrange
 		filepath := "sample.txt"
 		expected := DB{
-			ingredientRange: [][2]int{
+			ingredientRanges: [][2]int{
 				{3, 5},
 				{10, 14},
 				{16, 20},
@@ -24,6 +24,28 @@ func TestParseInput(t *testing.T) {
 
 		// Assert
 		if !reflect.DeepEqual(actual, expected) {
+			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
+		}
+	})
+}
+
+func TestIsFresh(t *testing.T) {
+	t.Run("when in range then returns true", func(t *testing.T) {
+		// Arrange
+		ingredientRanges := [][2]int{
+			{3, 5},
+			{10, 14},
+			{16, 20},
+			{12, 18},
+		}
+		ingredientId := 5
+		expected := true
+
+		// Act
+		actual := isFresh(ingredientRanges, ingredientId)
+
+		// Assert
+		if actual != expected {
 			t.Fatalf("\nactual=\n%#v\nexpected=\n%#v\n", actual, expected)
 		}
 	})
