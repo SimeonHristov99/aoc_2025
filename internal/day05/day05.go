9a182163e0ddafb50e66f10f0eb768900ca3739c
SimeonHristov99 <s.e.hristov99@gmail.com>
2025-12-20 14:58:52 +0200

    Remove printing.

--- internal/day05/day05.go
+++ internal/day05/day05.go
@@ -1,7 +1,6 @@
 package day05
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -44,7 +43,6 @@ func unionSize(lhs [2]int, rhs [2]int) int {
 		return unionSize(rhs, lhs)
 	}
 	if rhs[0] <= lhs[1] {
-		fmt.Println("left", lhs[1]-lhs[0]+1, "right", rhs[1], lhs[1]+1, rhs[1]-lhs[1]-1+1)
 		return lhs[1] - lhs[0] + 1 + rhs[1] - lhs[1] - 1 + 1
 	}
 	return lhs[1] - lhs[0] + 1 + rhs[1] - rhs[0] + 1
